b08a3112e1cd5528c8619611166d03a9d7e5d907
Aman Kumar <kumaraman140795@gmail.com>
2018-12-12 03:09:17 +0530

    Added markup and style for Header component

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -5,7 +5,12 @@ class Header extends React.Component{
   render(){
     return (
       <header>
-        <p>This is the header component</p>
+        <h1 id='search-title'>What would you like to read today?</h1>
+        <form id='search-form'>
+          <label htmlFor='search-input' id='search-label'>Type to search: </label>
+          <input type='text' id='search-input' placeholder='type keyword to search' autoFocus/>
+          <input type='submit' id='search-submit' />
+        </form>
       </header>
     );
   }
