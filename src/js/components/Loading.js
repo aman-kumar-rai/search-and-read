4b532c7b9ee2d5d60a9a399adc9d312201944755
Aman Kumar <kumaraman140795@gmail.com>
2018-12-13 01:09:14 +0530

    Adding gif and text for Loading component

--- src/js/components/Loading.js
+++ src/js/components/Loading.js
@@ -1,7 +1,17 @@
 const React = require("react");
 
 const Loading = props => {
-  return <h1>Loading...</h1>;
+  return (
+    <div id='loading'>
+      <img src='./../src/images/loading.gif' alt='loading' />
+      <p id='loading-text'>{props.loadingText}</p>
+    </div>
+  );
 };
+
+// setting the default props for the loading component...
+Loading.defaultProps = {
+  loadingText: 'Loading'
+}
 
 module.exports = Loading;
