a0b356d8c87f713a5c3d4bb45e04e6761642efed
Aman Kumar <kumaraman140795@gmail.com>
2018-12-12 10:55:27 +0530

    Added article rendering fallback

--- src/js/components/ArticleList.js
+++ src/js/components/ArticleList.js
@@ -7,12 +7,14 @@ class ArticleList extends React.Component {
   render() {
     return (
       <section id="articles-container">
-      {
-        //looping over the docs array to render an Article component for each... 
-      }
-        { this.props.docs.map(doc => {
+
+        {
+          this.props.docs.length == 0 
+          ? <div>No articles to show</div>
+          : this.props.docs.map(doc => {
             return <Article snippet={doc.snippet} url={doc.web_url}  key={doc._id} heading={doc.headline.main} />
           })
+        
         }
       </section>
     );
